2bf3055bd7d84dc47ceaa2edc104237fee2f2ea6
Alecio Barreto <aleciobarretu@gmail.com>
2024-09-12 00:12:16 -0300

    fix: remove assets from map

--- lib/app/core/utils/assets/png_assets.dart
+++
@@ -1,6 +0,0 @@
-class PngAssets {
-  static const String pin = 'assets/maps/tracker_pin.png';
-  static const String arrow = 'assets/maps/arrow.png';
-  static const String myLocation = 'assets/maps/my_position.png';
-  static const String trashPin = 'assets/maps/trash.png';
-}
