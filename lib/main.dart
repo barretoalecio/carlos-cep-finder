e71eaa94a26618aa3faa836576bc9b2c5cfed9ea
Alecio Barreto <aleciobarretu@gmail.com>
2024-09-17 10:37:18 -0300

    feat: add dotenv feature and hive in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'app/core/utils/configuration/hive_initialization.dart';
 import 'app/modules/app_module.dart';
 import 'app/modules/app_widget.dart';
 
+Future<void> loadEnviromentVariables() async {
+  await dotenv.load(fileName: '.env');
+}
+
 Future<void> main() async {
+  await loadEnviromentVariables();
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    ModularApp(
-      module: AppModule(),
-      child: const AppWidget(),
+  hiveInitialization(
+    onInitialized: () => runApp(
+      ModularApp(
+        module: AppModule(),
+        child: const AppWidget(),
+      ),
     ),
   );
 }
