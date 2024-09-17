f4ec8e6d4f29badacd41e05e34278b9610a50818
Alecio Barreto <aleciobarretu@gmail.com>
2024-09-17 10:45:48 -0300

    feat: implementing theme

--- lib/app/core/utils/themes/konsi_theme.dart
+++ lib/app/core/utils/themes/konsi_theme.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 final konsiTheme = ThemeData(
-  scaffoldBackgroundColor: const Color(0xFFFFFFFF),
+  fontFamily: 'roboto',
+  scaffoldBackgroundColor: const Color(0xFFF6F6F6),
   appBarTheme: const AppBarTheme(
-    backgroundColor: Colors.white,
+    backgroundColor: Color(0xFFF6F6F6),
     surfaceTintColor: Colors.transparent,
     scrolledUnderElevation: 0,
     centerTitle: true,
@@ -11,13 +12,15 @@ final konsiTheme = ThemeData(
   ),
   colorScheme: const ColorScheme(
     primary: Color(0xFF2E8896),
+    onPrimaryContainer: Color(0xFFFCA33B),
     secondary: Color(0xFFE8E8E8),
     surface: Color(0xFFFFFFFF),
     error: Color(0xFFEB5757),
     onPrimary: Color(0xFFEBF5F7),
     onSecondary: Color(0xFF7C7C7C),
-    onSurface: Color(0xFFFFFFFF),
+    onSurface: Color(0xFFFBFBFB),
     onError: Color(0xFFFFFFFF),
+    onSecondaryFixed: Color(0xFF5E6772),
     brightness: Brightness.light,
   ),
   textTheme: TextTheme(
